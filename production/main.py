683756833e1a3cc402a347fe57bcde9b472b4380
AMNbot <aymenouali01@gmail.com>
2022-10-02 01:36:38 -0400

    first commit

---
+++ production/main.py
@@ -0,0 +1,59 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+import torch
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_pose = mp.solutions.pose
+
+
+model = torch.jit.load('model_scripted.ptrom')
+model.eval()
+
+# For webcam input:
+cap = cv2.VideoCapture(0)
+with mp_pose.Pose(
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            print("Ignoring empty camera frame.")
+            # If loading a video, use 'break' instead of 'continue'.
+            continue
+
+        try:
+            # To improve performance, optionally mark the image as not writeable to
+            # pass by reference.
+            image.flags.writeable = False
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            results = pose.process(image)
+
+            pose_landmarks = results.pose_landmarks
+            data = np.zeros((33, 4))
+            for i, data_point in enumerate(pose_landmarks.landmark):
+                if data_point.visibility >= 0.5:
+                    data[i][0] = data_point.x
+                    data[i][1] = data_point.y
+                    data[i][2] = data_point.z
+                    data[i][3] = data_point.visibility
+
+            input = torch.tensor(data).flatten()
+            out = model.forward(torch.reshape(input, (1, -1)))
+            print(torch.argmax(out))
+
+            # Draw the pose annotation on the image.
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            mp_drawing.draw_landmarks(
+                image,
+                results.pose_landmarks,
+                mp_pose.POSE_CONNECTIONS,
+                landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+            # Flip the image horizontally for a selfie-view display.
+            cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
+            if cv2.waitKey(5) & 0xFF == 27:
+                break
+        except:
+            print("Ya r fdp qcq tu me racontes la")
+cap.release()
