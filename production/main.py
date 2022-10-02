4135b5f7f179f70c998266b4e175417ef2e360b0
Jun Kai Liao <2003junkai@gmail.com>
2022-10-02 19:42:20 -0400

    bugfix and remove sleep

--- production/main.py
+++ production/main.py
@@ -20,7 +20,7 @@ mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
 
 
-model = torch.jit.load('production/model_scripted.ptrom')
+model = torch.jit.load('model_scripted.ptrom')
 model.eval()
 
 # For webcam input:
@@ -38,14 +38,15 @@ with mp_pose.Pose(
             # If loading a video, use 'break' instead of 'continue'.
             continue
 
-        try:
-            # To improve performance, optionally mark the image as not writeable to
-            # pass by reference.
-            image.flags.writeable = False
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            results = pose.process(image)
+        # To improve performance, optionally mark the image as not writeable to
+        # pass by reference.
+        image.flags.writeable = False
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = pose.process(image)
 
-            pose_landmarks = results.pose_landmarks
+        pose_landmarks = results.pose_landmarks
+           
+        try:
             data = np.zeros((33, 4))
             for i, data_point in enumerate(pose_landmarks.landmark):
                 if data_point.visibility >= 0.5:
@@ -53,46 +54,47 @@ with mp_pose.Pose(
                     data[i][1] = data_point.y
                     data[i][2] = data_point.z
                     data[i][3] = data_point.visibility
-
-            input = torch.tensor(data).flatten()
-            out = model.forward(torch.reshape(input, (1, -1)))
-            out = torch.argmax(out)
-            # print(out)
-
-            # Draw the pose annotation on the image.
-            image.flags.writeable = True
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-            mp_drawing.draw_landmarks(
-                image,
-                results.pose_landmarks,
-                mp_pose.POSE_CONNECTIONS,
-                landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-            # Flip the image horizontally for a selfie-view display.
-            cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
-            if cv2.waitKey(5) & 0xFF == 27:
-                break
-            time.sleep(1)
-
-            lst[rounds] = out
-            print(lst)
-            # print(out)
-            if rounds == len(lst) - 1:
-                print("slt ben genre t juste nul nn?")
-                if np.mean(lst) >= 0.8:
-                    message = client.messages \
-                        .create(
-                            body='You should fix your posture, it is pretty bad!',
-                            from_='+17402736640',
-                            to='+15147028472'
-                        )
-
-                    print(message.sid)
-
-                lst = np.zeros((rlen))
-                rounds = 0
-            rounds += 1
         except:
-            print("Ya r fdp qcq tu me racontes la")
+            continue
+
+        input = torch.tensor(data).flatten().float()
+        out = model.forward(torch.reshape(input, (1, -1)))
+        out = torch.argmax(out)
+        # print(out)
+
+        # Draw the pose annotation on the image.
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        mp_drawing.draw_landmarks(
+            image,
+            results.pose_landmarks,
+            mp_pose.POSE_CONNECTIONS,
+            landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+        # Flip the image horizontally for a selfie-view display.
+        cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
+        if cv2.waitKey(5) & 0xFF == 27:
+            break
+        #time.sleep(1)
+
+        lst[rounds] = out
+        print(out)
+        # print(out)
+        rounds += 1
+        if rounds == len(lst):
+            if np.mean(lst) >= 0.8:
+                print("Bad posture")
+                '''
+                message = client.messages \
+                    .create(
+                        body='You should fix your posture, it is pretty bad!',
+                        from_='+17402736640',
+                        to='+15147028472'
+                    )
+
+                print(message.sid)
+'''
+            lst = np.zeros((rlen))
+            rounds = 0
             
 
 cap.release()
